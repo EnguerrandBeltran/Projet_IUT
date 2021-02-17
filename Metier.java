1e5d64c2ebe8a5f08dd53f12c256e8e63dd4b277
duchess <duchess@pop-duchess>
2021-02-17 14:41:06 +0100

    Update Metier.java

---
+++ Metier.java
@@ -0,0 +1,34 @@
+public class Metier
+{
+	private int val;
+
+	public Metier()
+	{
+		this.val = 0;
+	}
+
+	public void augmenter(char[] tabCara, int lig, int col,char dir)
+	{
+		char temp;
+		temp = tabCara[lig][col]
+		if(dir == 'D')
+		{
+			
+		}
+		else
+		{
+			
+		}
+		
+	}
+
+	public void diminuer()
+	{
+		this.val--;
+
+		if ( this.val == -11 ) this.val = 10;
+	}
+
+	public int getVal(){ return this.val; }
+
+}
